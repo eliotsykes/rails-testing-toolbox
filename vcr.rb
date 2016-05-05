289a194491a113b9acac4f3f9b8875e5e9a93414
Eliot Sykes <eliotsykes@gmail.com>
2016-05-05 15:24:45 +0100

    Introduce replace_uri_credentials in VCR config

--- vcr.rb
+++ vcr.rb
@@ -11,6 +11,11 @@ VCR.configure do |config|
     match_requests_on: [:method, :uri, :body],
     record: record_mode
   }
+
+  # Temporarily enable config.debug_logger when debugging VCR:
+  # config.debug_logger = File.open(Rails.root.join('log', 'vcr.log'), 'w')
+  # config.debug_logger = STDOUT
+
   config.cassette_library_dir = "spec/support/http_cache/server"
   config.hook_into :webmock
 
@@ -19,6 +24,38 @@ VCR.configure do |config|
 
   config.allow_http_connections_when_no_cassette = false
 
-  # Temporarily enable config.debug_logger when debugging VCR:
-  config.debug_logger = File.open(Rails.root.join('log', 'vcr.log'), 'w')
+  # Replace sensitive data before recording cassette
+  config.before_record do |http_interaction|
+    sensitive_request_headers = ['Authorization']
+    sensitive_request_headers.each do |header_key|
+      if http_interaction.request.headers.key?(header_key)
+        raise "Handle protecting #{header_key} request header in VCR"
+      end
+    end
+
+    sensitive_response_headers = ['Set-Cookie']
+    sensitive_response_headers.each do |header_key|
+      if http_interaction.response.headers.key?(header_key)
+        raise "Handle protecting #{header_key} response header in VCR"
+      end
+    end
+
+    def replace_uri_credentials(http_interaction)
+      dummy_credentials_by_host_pattern = {
+        /example\.com/ => 'EXAMPLE_USER_FOR_TEST:EXAMPLE_SECRET_FOR_TEST',
+        /foo\.com/ => 'FOO_USER_FOR_TEST:FOO_SECRET_FOR_TEST'
+      }
+
+      http_interaction.request.uri.sub!(%r{(?<=://).*:.*(?=@)}) do
+        host_and_path = $'
+        result = dummy_credentials_by_host_pattern.find do |(host_pattern, credentials)|
+          host_pattern === host_and_path
+        end
+        raise "Not yet able to handle dummy credentials for #{host_and_path}" if result.blank?
+        dummy_credentials = result.last
+      end
+    end
+
+    replace_uri_credentials(http_interaction)
+  end
 end
