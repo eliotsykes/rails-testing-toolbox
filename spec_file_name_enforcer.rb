0ded749521c2bcfbffaf4e3d2e614d1747b4e955
Eliot Sykes <eliotsykes@gmail.com>
2016-05-19 22:06:19 +0100

    Ignore spec/*/shared_examples/* files in spec file name enforcer

--- spec_file_name_enforcer.rb
+++ spec_file_name_enforcer.rb
@@ -7,7 +7,7 @@ RSpec.configure do |config|
       # non-spec file paths under spec/ will not raise an error.
       File.file?(filename) &&
         !filename.match(/_spec\.rb\z/) &&
-        !filename.match(%r{\Aspec/(support|factories|mailers/previews)/}) &&
+        !filename.match(%r{\Aspec/(support|factories|mailers/previews|[a-z]+/shared_examples)/}) &&
         !filename.match(%r{\Aspec/(spec_helper\.rb|rails_helper\.rb|examples\.txt)\z})
     end
 
