6265a4c0d6040b1b7473f751a8e4b8238a407842
Eliot Sykes <eliotsykes@gmail.com>
2016-05-25 08:21:01 +0100

    Add card_number method to StripeHelper

--- stripe_helper.rb
+++ stripe_helper.rb
@@ -1,5 +1,15 @@
 module StripeHelper
 
+  # See https://stripe.com/docs/testing for more card numbers:
+  CARD_NUMBERS = {
+    visa: '4242424242424242',
+    mastercard: '5555555555554444'
+  }
+
+  def card_number(type)
+    CARD_NUMBERS[type]
+  end
+
   def fill_in_with_force(locator, options)
     page.execute_script "$('#{locator}').val('#{options[:with]}');"
   end
