de29bd60af5754fda0021798d8a4c146cf56f36e
Eliot Sykes <eliotsykes@gmail.com>
2016-05-25 06:48:03 +0100

    CI env check in capybara.rb to extend wait time

--- capybara.rb
+++ capybara.rb
@@ -1,2 +1,9 @@
 require 'capybara/rails'
 require 'capybara/rspec'
+
+# Capybara defaults to 2 secs, increase on CI env to reduce false
+# negative failures.
+if ENV['CONTINUOUS_INTEGRATION'] || ENV['CI']
+  Capybara.default_max_wait_time = 3.seconds
+  puts "[Capybara] Using #{Capybara.default_max_wait_time} seconds max wait time"
+end
