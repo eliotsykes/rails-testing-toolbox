fc0ca41ee20edfca7b306add95eac6b3393fc0be
Eliot Sykes <eliotsykes@gmail.com>
2016-06-15 15:36:40 +0100

    Improve reliability of ahoy matcher have_tracked_event

--- ahoy_matchers.rb
+++ ahoy_matchers.rb
@@ -1,11 +1,20 @@
 require_relative 'matcher_extensions'
 
 RSpec::Matchers.define :have_tracked_event do |name, user: nil, **props|
-  match_unless_raises do |_|
+
+  supports_block_expectations
+
+  match_unless_raises do |interaction|
+    is_ahoy_ready_snippet = "typeof ahoy !== 'undefined' && ahoy !== null;"
+    RSpec::Wait.wait_for { evaluate_script(is_ahoy_ready_snippet) }.to eq true
+
+    interaction.call
+
     relation = Ahoy::Event.where(name: name, user_id: user.try(:id))
     props.each do |key, value|
       relation = relation.where("properties ->> ? = ?", key, value)
     end
+
     RSpec::Wait.wait_for { relation.exists? }.to eq true
   end
 end
