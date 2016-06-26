0961cf80d3bc7db9bfe3124f37b312376114e608
Eliot Sykes <eliotsykes@gmail.com>
2016-06-26 21:16:09 +0100

    email_spec/rspec does everything needed - no need for RSpec.configure block

--- email_spec.rb
+++ email_spec.rb
@@ -1,11 +1,2 @@
 require 'email_spec'
-
-RSpec.configure do |config|
-  config.include EmailSpec::Helpers
-  config.include EmailSpec::Matchers
-
-  config.before(:each) do
-    reset_mailer # Clears out ActionMailer::Base.deliveries
-  end
-
-end
+require 'email_spec/rspec'
