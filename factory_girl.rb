b1a8db1f59ec84e1dc8ceb1ff7e43b22d64caeef
Eliot Sykes <eliotsykes@gmail.com>
2016-11-17 13:05:12 +0000

    FactoryGirl config for factories needing some TLC

--- factory_girl.rb
+++ factory_girl.rb
@@ -5,6 +5,11 @@ RSpec.configure do |config|
     begin
       DatabaseCleaner.start
       FactoryGirl.lint traits:true
+      factories_needing_tlc = []
+      factories_to_lint = FactoryGirl.factories.reject do |factory|
+        factories_needing_tlc.include?(factory.name)
+      end
+      FactoryGirl.lint(factories_to_lint, traits: true)
     ensure
       DatabaseCleaner.clean
     end
