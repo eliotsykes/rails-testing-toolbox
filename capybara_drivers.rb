4408e49ea1138ddfd8d5fe4aec13654d009b702e
Eliot Sykes <eliotsykes@gmail.com>
2016-11-22 12:16:48 +0000

    Add selenium_chrome Capybara driver

--- capybara_drivers.rb
+++ capybara_drivers.rb
@@ -1,3 +1,7 @@
+Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
 Capybara.register_driver :selenium_chrome_ssl do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(accept_ssl_certs: true)
   Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
