fa794ca6ed3fc210a17e8e58906bb8dce81a7012
Eliot Sykes <eliotsykes@gmail.com>
2016-12-08 10:55:06 +0000

    print_chromedriver_help_if_needed

--- capybara_drivers.rb
+++ capybara_drivers.rb
@@ -6,3 +6,22 @@ Capybara.register_driver :selenium_chrome_ssl do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(accept_ssl_certs: true)
   Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
 end
+
+def print_chromedriver_help_if_needed(exception)
+  return if exception.nil?
+  chromedriver_needs_installing =
+    exception.is_a?(Selenium::WebDriver::Error::WebDriverError) &&
+    /chromedriver/i === exception.message
+
+    puts '-'*80
+    puts 'chromedriver is not installed, please install to run JS-dependent specs:'
+    puts '  - On macOS `brew install chromedriver`'
+    puts '-'*80
+end
+
+RSpec.configure do |config|
+  config.around(:each, type: :feature) do |example|
+    example.run
+    print_chromedriver_help_if_needed(example.exception)
+  end
+end
