c93c072ce423030fc130b9f3575f8c7b30721450
Eliot Sykes <eliotsykes@gmail.com>
2016-12-08 10:56:36 +0000

    Fix chromedriver install help method

--- capybara_drivers.rb
+++ capybara_drivers.rb
@@ -9,14 +9,17 @@ end
 
 def print_chromedriver_help_if_needed(exception)
   return if exception.nil?
+
   chromedriver_needs_installing =
     exception.is_a?(Selenium::WebDriver::Error::WebDriverError) &&
     /chromedriver/i === exception.message
 
-    puts '-'*80
-    puts 'chromedriver is not installed, please install to run JS-dependent specs:'
-    puts '  - On macOS `brew install chromedriver`'
-    puts '-'*80
+  return unless chromedriver_needs_installing
+
+  puts '-'*80
+  puts 'chromedriver is not installed, please install to run JS-dependent specs:'
+  puts '  - On macOS `brew install chromedriver`'
+  puts '-'*80
 end
 
 RSpec.configure do |config|
