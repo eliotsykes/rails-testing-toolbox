c8c5d2828c58792938b1ff7892d53b04ca24e907
Eliot Sykes <eliotsykes@gmail.com>
2017-01-30 17:46:15 +0000

    Notes on introducing FactoryGirl into legacy codebases

--- factory_girl.rb
+++ factory_girl.rb
@@ -4,12 +4,16 @@ RSpec.configure do |config|
   config.before(:suite) do
     begin
       DatabaseCleaner.start
-      FactoryGirl.lint traits:true
-      factories_needing_tlc = []
-      factories_to_lint = FactoryGirl.factories.reject do |factory|
-        factories_needing_tlc.include?(factory.name)
-      end
-      FactoryGirl.lint(factories_to_lint, traits: true)
+
+      # Option 1) Lint all factories with traits:
+      FactoryGirl.lint traits: true
+
+      # OR Option 2) Lint selected factories (useful for legacy codebases):
+      # factories_needing_tlc = [] # List factories that aren't working yet
+      # factories_to_lint = FactoryGirl.factories.reject do |factory|
+      #   factories_needing_tlc.include?(factory.name)
+      # end
+      # FactoryGirl.lint(factories_to_lint, traits: true)
     ensure
       DatabaseCleaner.clean
     end
