966b84df9baf3e3a3f7d4fc4a006edc4318b7c3a
Eliot Sykes <eliotsykes@gmail.com>
2017-04-01 14:54:36 +0100

    Paperclip::Shoulda::Matchers config

--- paperclip.rb
+++ paperclip.rb
@@ -1,6 +1,10 @@
+require 'paperclip/matchers'
+
 # See: https://github.com/thoughtbot/paperclip#testing
 # Assumes default paperclip config for :filesystem storage in test env.
 RSpec.configure do |config|
+  config.include Paperclip::Shoulda::Matchers
+
   config.after(:suite) do
     FileUtils.rm_rf(Dir["#{Rails.root}/public/system/"])
   end
