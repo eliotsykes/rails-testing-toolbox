46ea5e30e4d3798327b4bab3fac2ae2ed82aba7b
Eliot Sykes <eliotsykes@gmail.com>
2017-05-19 15:34:55 +0100

    Accept RECORD=1 env var for enabling HttpRecord

--- http_record.rb
+++ http_record.rb
@@ -1,4 +1,4 @@
-# To enable recording HTTP interactions temporarily, run the 
+# To enable recording HTTP interactions temporarily, run the
 # individual spec and prepend with RECORD set to true, e.g.:
 # RECORD=true bin/rspec spec/features/user_upgrades_spec.rb
 
@@ -6,8 +6,8 @@ module HttpRecord
   def self.off?
     !on?
   end
-  
+
   def self.on?
-    'true' == ENV['RECORD']
+    ['true', '1'].include? ENV['RECORD']
   end
 end
