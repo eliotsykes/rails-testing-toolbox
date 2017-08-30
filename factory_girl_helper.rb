66b068c6848c39f7873d2d7bc48ce4c556601273
Eliot Sykes <eliotsykes@gmail.com>
2017-08-30 17:43:02 +0100

    Document sample usage of strategy_method with factory

--- factory_girl_helper.rb
+++ factory_girl_helper.rb
@@ -1,4 +1,12 @@
 module FactoryGirlHelper
+  # Sample Usage:
+  #
+  # after(:stub, :build) do |account, evaluator|
+  #   strategy_method = strategy_method(evaluator)
+  #
+  #   # Calls build_stubbed, build, or create on the user factory with given arguments:
+  #   account.owner ||= send(strategy_method, :user, :some_user_trait, email: 'foo@example.org')
+  # end
   def strategy_method(evaluator)
     strategy_method =
       evaluator.instance_variable_get(:@build_strategy).class.name.demodulize.downcase
