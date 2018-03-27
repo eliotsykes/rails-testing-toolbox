b1ea49595d676657323a1e1419bbd0328a4c989a
Eliot Sykes <eliotsykes@gmail.com>
2018-03-27 18:09:55 +0100

    Do not run delayed jobs scheduled in future, introduce run_next_job

--- delayed_job_helper.rb
+++ delayed_job_helper.rb
@@ -2,12 +2,33 @@
 # - 'run_jobs: true' to run jobs inline immediately
 # - 'run_jobs: false' to store jobs in delayed_jobs table, and *not* run the jobs
 module DelayedJobHelper
+
+  DELAY_IF_JOB_SCHEDULED_IN_FUTURE = ->(job) { job.run_at.try(:future?) }
+  DELAY_ALL_JOBS = true
+
   def run_jobs(perform)
     original_delay_jobs = Delayed::Worker.delay_jobs
-    Delayed::Worker.delay_jobs = !perform
+
+    if perform
+      Delayed::Worker.delay_jobs = DELAY_IF_JOB_SCHEDULED_IN_FUTURE
+    else
+      Delayed::Worker.delay_jobs = DELAY_ALL_JOBS
+    end
+
     yield
   ensure
     Delayed::Worker.delay_jobs = original_delay_jobs
+  end
+
+  def run_next_job(allow_failure: false)
+    num_jobs_to_run = 1
+    success_count, failure_count = Delayed::Worker.new.work_off(num_jobs_to_run)
+    expect(success_count + failure_count).to eq(1), 'should have tried to perform one job'
+
+    if !allow_failure
+      expect(success_count).to eq(1), 'job should have run successfully'
+      expect(failure_count).to eq(0), 'unexpected job failure'
+    end
   end
 end
 
