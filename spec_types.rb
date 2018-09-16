f550e5dd8779acfbcc108dece45de151a8298456
Eliot Sykes <eliotsykes@gmail.com>
2018-09-16 11:13:10 +0100

    Add service spec type

--- spec_types.rb
+++ spec_types.rb
@@ -1,11 +1,14 @@
+# Define custom spec types not provided by rspec-rails
 RSpec.configure do |config|
-  # Define custom spec types not provided by rspec-rails:
-  config.define_derived_metadata(:file_path => %r{/spec/validators/}) do |metadata|
-    metadata[:type] = :validator
-  end
+  spec_types = [
+    'service',
+    'task', # Rake tasks
+    'validator'
+  ]
 
-  # Rake tasks
-  config.define_derived_metadata(:file_path => %r{/spec/tasks/}) do |metadata|
-    metadata[:type] = :task
+  spec_types.each do |spec_type|
+    config.define_derived_metadata(file_path: %r{/spec/#{spec_type.pluralize}/}) do |metadata|
+      metadata[:type] = spec_type
+    end
   end
 end
